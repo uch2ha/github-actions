a922bf3dcded4cc64031d2540fb9f643b19e13e9
Dmitry Sinyavskiy <sinyavskiy.d@gmail.com>
2022-02-06 14:22:37 +0200

    1

--- postinumerot.py
+++ postinumerot.py
@@ -1,1 +1,46 @@
-# Kopioi aikaisempi ratkaisusi tänne. Lisää tarvittaessa myös muut ratkaisusi tiedostot.
+# Esimerkkisuoritus:
+#
+# Kirjoita postitoimipaikka: Porvoo
+# Postinumerot: 06100, 06101, 06150, 06151, 06200, 06400, 06401, 06450, 06500
+
+
+import json
+from typing import Optional
+
+
+def get_postalcode_by_post_name(code: str)-> Optional[str]:
+
+    f = open("postcode_map_light.json")
+    data = json.load(f)
+
+    answer = []
+    answerStr = ""
+
+    for key, value in data.items():
+        if code.upper() == value:
+            answer.append(key)
+
+    answer.sort()
+
+    for i in answer:
+        answerStr += i+", "
+
+    return answerStr[:-2]
+
+    f.close()
+
+if __name__ == "__main__":
+
+    post_place = (input("Kirjoita postitoimipaikka:"))
+
+
+    answer = get_postalcode_by_post_name(post_place)
+
+    if answer:
+        print("Postinumerot: ", end="")
+        print(answer)
+    else:
+        print("Tuntematon postitoimipaikka")  
+
+
+
--- postinumerot_test.py
+++ postinumerot_test.py
@@ -1,1 +1,13 @@
 # Kirjoita postinumerot-moduulin testit tähän tiedostoon
+# from postinumerot import get_postalcode_by_post_name
+from postinumerot import get_postalcode_by_post_name
+
+
+def test_finding_kainasto():
+    assert get_postalcode_by_post_name("KAINASTO") == "61820"
+
+def test_finding_nothing():
+    assert get_postalcode_by_post_name("KIATO") == ""
+
+def test_finding_teuva():
+    assert get_postalcode_by_post_name("teuva") == "64700, 64701"
